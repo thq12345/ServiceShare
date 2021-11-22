08d6ea55329ff530240d708c0ca340467ae122f3
thq12345 <qu.t@northeastern.edu>
2021-11-22 13:09:00 -0500

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.use("/api", indexRouter);
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
 });
 
 module.exports = app;
