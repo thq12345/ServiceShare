686b8e4ad8fc9c59fe2e46ba0f955fcd673516cd
John Alexis Guerra Gómez <john.guerra@gmail.com>
2021-12-08 16:50:23 -0800

    Ugly hack for fixing the aria-roledescription

--- frontend/src/Other Components/map.js
+++ frontend/src/Other Components/map.js
@@ -1,7 +1,27 @@
-import React from "react";
+import {React, useEffect, useRef} from "react";
 import GoogleMapReact from "google-map-react";
 import PropTypes from "prop-types";
 require("dotenv").config();
+
+function Deleter() {
+  // Ugly hack to make google maps pass the axe audit
+  const mapRef = useRef();
+  useEffect(() => {
+    console.log("ref", mapRef.current);
+
+    try {
+      mapRef.current.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.removeAttribute("aria-roledescription")
+      console.log("removing aria-roledescription", mapRef.current);
+    } catch (er) {
+      console.log("deleter failed, maybe the map wasn't ready?", er);
+    }
+
+
+  }, []);
+
+
+  return <div ref={mapRef}></div>
+}
 
 const CenterRedCirclePinpoint = () => (
   <div
@@ -29,15 +49,19 @@ function Map(props) {
     },
     zoom: 11,
   };
+
+
+
   // document.getElementsByTagName("GoogleMapReact")[0].removeAttribute("class");
   return (
-    <div className={"map"} role="application">
+    <div className={"map"}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
         <CenterRedCirclePinpoint lat={props.latitude} lng={props.longitude} />
+        <Deleter></Deleter>
       </GoogleMapReact>
     </div>
   );
